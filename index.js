8059568d7c5fcce80ea2bceef9f003937e23390b
shawnconway <shawnway210@gmail.com>
2023-08-22 10:00:35 -0400

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName = 'BOB'
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+const leastFavoriteCustomer = "John"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Jane"
+}
+changeLeasrFavoriteCustomer()
